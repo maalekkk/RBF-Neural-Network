6865790d0624cca8cf53ab2ac253bc3e22401732
maalekkk <bartek.malkowski97@gmail.com>
2020-05-31 21:12:17 +0200

    fix kMeans

--- kMeans.py
+++ kMeans.py
@@ -9,22 +9,21 @@ def get_distance(x1, x2):
 
 
 def k_means(data, num_of_centers):
-    centroids = data[np.random.choice(range(len(data)), num_of_centers)]
-    converged = False
-    current_iter = 0
-    while (not converged) and (current_iter < 1000):
-        cluster_list = [[] for i in range(len(centroids))]
+    centers = data[np.random.choice(range(len(data)), num_of_centers)]
+    equals = False
+    iter = 0
+    while (not equals) and (iter < 1000):
+        cluster_list = [[] for i in range(len(centers))]
         for x in data:
             distances_list = []
-            for c in centroids:
+            for c in centers:
                 distances_list.append(get_distance(c, x))
             cluster_list[int(np.argmin(distances_list))].append(x)
         cluster_list = list((filter(None, cluster_list)))
-        prev_centroids = centroids.copy()
-        centroids = []
+        prev_centers = centers.copy()
+        centers = []
         for j in range(len(cluster_list)):
-            centroids.append(np.mean(cluster_list[j], axis=0))
-        pattern = np.abs(np.sum(prev_centroids) - np.sum(centroids))
-        converged = (pattern == 0)
-        current_iter += 1
-    return np.array(centroids)
+            centers.append(np.mean(cluster_list[j], axis=0))
+        equals = (np.abs(np.sum(prev_centers) - np.sum(centers)) == 0)
+        iter += 1
+    return np.array(centers)
